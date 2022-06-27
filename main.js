90872c521ef969e335d3fc09b3ede31ec100a880
Syed Shahid Ali <syedshahid1215@gmail.com>
2022-06-27 18:28:01 +0500

    add threejs helpers and orbit controls

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "./style.css"
 
 import * as THREE from "three"
 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -30,6 +32,11 @@ const ambientLight = new THREE.AmbientLight(0xffffff)
 
 scene.add(pointLight, ambientLight)
 
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper  = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
 // renderer.render(scene, camera)
 
 // we dont wnna call render method over and over again
@@ -41,6 +48,8 @@ function animate() {
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
+
+  controls.update()
   // whenever browser repants the screen it will call render method to update the UI,Game loop
   renderer.render(scene, camera)
 
