2bf82a7e233d2d703bce9c765c0be56be8b9b3ad
Syed Shahid Ali <syedshahid1215@gmail.com>
2022-06-27 14:37:34 +0500

    add geomtry, mesh and material

--- main.js
+++ main.js
@@ -16,3 +16,11 @@ renderer.setSize(window.innerWidth, window.innerHeight) // full screen
 camera.position.setZ(30)
 
 renderer.render(scene, camera) // draw
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100) // just like a big 3d ring
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true })
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+renderer.render(scene, camera)
